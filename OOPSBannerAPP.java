733b932c393c2918b04f7b74c86abed7d5b35654
EnviZity <rr3060@srmist.edu.in>
2026-02-16 19:36:44 +0530

    Added gitignore

---
+++ OOPSBannerAPP.java
@@ -0,0 +1,13 @@
+/**
+*OOPSBannerApp UC1 - OOPS Banner Display Application
+*
+*This class demonstrates a simple Java application that displays the object Oriented Programmaing System OOPS acronym to console
+*
+*@author Devoloper
+*@verson 1.0
+*/
+public class OOPSBannerAPP {
+public static void main(String[] args){
+  System.out.println("OOPS");
+  }
+}
